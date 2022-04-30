1f02ce03e511d1d9106c51a016d22103093f19fe
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2022-04-30 12:18:58 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,12 +1,26 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+# options = webdriver.ChromeOptions()
+# options.add_argument("user-data-dir=/Users/kelvintam/Library/Application Support/Google/Chrome")
+# options.add_argument('--profile-directory=Profile 1')
+# options.page_load_strategy = 'normal'
 
-driver.get("https://www.google.com.hk")
+# browser = webdriver.Chrome(chrome_options=options)
+browser = webdriver.Chrome()
+browser.get("https://www.google.com")
 
-searchBar = driver.find_element_by_class_name("gLFyf.gsfi")
-button = driver.find_element_by_class_name("gNO89b")
-
-searchBar.send_keys('Hello World')
+searchInput = browser.find_element(By.CLASS_NAME, "gLFyf.gsfi")
+button = browser.find_element(By.CLASS_NAME, "gNO89b")
+searchInput.send_keys("Half Life 3 when")
+sleep(0.1)
 button.click()
+
+link = browser.find_element(By.PARTIAL_LINK_TEXT, "reportedly")
+link.click()
+
+# /Users/kelvintam/Library/Application Support/Google/Chrome/Profile 1
+
+# browser = webdriver.Chrome(chrome_options=options)
